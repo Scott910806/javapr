0bf1d28488aa4487756931f9b9cb56bb69e3913b
yxma2010 <yxma2010@126.com>
2022-02-15 18:29:07 +0800

    close scanner

--- src/learnjava/baseclass/ScannerKeyBoardTest.java
+++ src/learnjava/baseclass/ScannerKeyBoardTest.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class ScannerKeyBoardTest {
     public static void main(String[] args) {
         // System.in 代表标准输入
-        var sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         // 使用分割符(使用换行符作为分割符，则将整行输出)
         sc.useDelimiter("\n");
         while(sc.hasNext()){
             System.out.println("键盘输入内容是：" + sc.next());
         }
+        sc.close();
     }
 }
